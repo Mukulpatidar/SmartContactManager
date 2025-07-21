1c91942a9e7e17a281d6163e31492e998c35bf52
Mukulpatidar <patidarmukul127.com>
2025-07-21 20:25:30 +0530

    Add base.html file for common page

--- src/main/java/com/smart/Controller/HomeController.java
+++ src/main/java/com/smart/Controller/HomeController.java
@@ -2,6 +2,7 @@ package com.smart.Controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,8 +13,14 @@ public class HomeController {
 	
 
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("title","Home - Smart Contact Manager");
 		return "home";
 	}
 	
+	@RequestMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title","About - Smart Contact Manager");
+		return "about";
+	}
 }

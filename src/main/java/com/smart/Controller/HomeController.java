178ccb573369c1bd2187b3f8215c15ed9c1fe98a
Mukulpatidar <patidarmukul127.com>
2025-07-22 23:43:38 +0530

    Register data in database

--- src/main/java/com/smart/Controller/HomeController.java
+++ src/main/java/com/smart/Controller/HomeController.java
@@ -1,18 +1,24 @@
 package com.smart.Controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.smart.Dao.UserRepository;
 import com.smart.entities.User;
 
 
 @Controller
 public class HomeController {
 	
-	
+	@Autowired
+	private UserRepository userRepository;
 
 	@RequestMapping("/")
 	public String home(Model model) {
@@ -26,6 +32,25 @@ public class HomeController {
 		model.addAttribute("user",new User());
 		return "signup";
 	}
+	
+	
+	//handler for registering user
+	
+	@PostMapping("/do_register")
+	public String registerUser(@ModelAttribute("user") User user,@RequestParam(value="agreement",defaultValue = "false") boolean agreement,Model model ) {
+		
+		user.setRole("ROLE_USER");
+		user.setEnabled(true);
+		
+		System.out.println(agreement);
+		System.out.println(user);
+		
+		
+		User result=this.userRepository.save(user);
+		
+		return "signup";
+	}
+	
 	
 	
 //	@RequestMapping("/login")
